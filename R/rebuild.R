45912ee173b5df1e23375cd86c16ae2e98c9fac1
Vaughan Roberts <vaughan@roberts.name>
2019-01-19 17:56:53 +1100

    Added the rebuild files into git

---
+++ R/rebuild.R
@@ -0,0 +1,119 @@
+#' Rebuild columns of the full results matrix after running forecasts on unique columns
+#' @param i is the column index into the full matrix of time-series
+#' @param y is the reduced matrix of forecasts
+#' @param u is a boolean vector indicating which columns in the full matrix are unique and need to be forecast
+#' @param m is a numerical vector which maps the removed columns to their equivalents in the full matrix
+#' 
+#' @return the next column of forecasts
+#' 
+#' @author Vaughan Roberts
+#' @seealso forecast.gts part of the hts2 package
+#' 
+#' @example
+#' seq(to=ncol(ally)) is the full set of indices into the vector
+#' of aggregated time-series (before forecast) as returned by aggts() function
+#' fally is the reduced matrix of hts forecasts which was originally created by
+#' fally <- ally[,uniqv]
+#' \dontrun{
+#' ally1 <- lapply(seq(to=ncol(ally)),FUN=rebuild,y=fally,u=uniqv,m=mapv)
+#' }
+
+rebuild <- function(i,y,u,m) {
+	mdebug <- TRUE
+	# y is now a list with each forecast as an element in the list
+	if (!u[i]) {
+		uc<-m[i]
+		# copy earlier column
+		if (mdebug) print(paste("rebuild: col ",i," adding back col ",uc))		
+		out <- if (uc>0) y[[uc]]
+	} else {
+		curr <- sum(u[1:i])
+		if (mdebug) print(paste("rebuild: col ",i," using col ",curr))		
+		out <- y[[curr]]
+	}
+	return(out)
+}
+
+#' Create supporting vectors for hts object so forecasts are only run on unique columns
+#' @param y is a matrix consisting of columns of time-series
+#' @return a list consisting of two named elements $uniq and $map
+#' @author Vaughan Roberts
+#' @seealso forecast.gts part of the hts2 package
+#' 
+#' @example
+#' red <- redparams(htseg3)
+#' \dontrun{
+#' fally <- ally[,red$uniq]
+#' ... do forecast loop in forecast.gts
+#' ally1 <- lapply(seq(to=ncol(ally)),FUN=rebuild,y=fally,u=red$uniq,m=red$map)
+#' }
+redparams <- function(y,nodes) {
+	if (!is.ts(y)) stop("Argument y must be a time-series object.", call. = FALSE)
+	uniqv <- makeuniqv(y,nodes)
+	mapv <- makemapv(y,nodes,uniqv)
+	
+	out <- setNames(list(uniqv, mapv), c("uniq", "map"))
+	return(out)	
+}
+
+makeuniqv <- function(y,nodes) {
+	if (!is.ts(y)) stop("Argument y must be a time-series object.", call. = FALSE)
+	mdebug <- FALSE
+	if (length(nodes[[1]]) == 1 && nodes[[1]][1] == 1) stop("Incorrect nodes list, the first element should not be 1", call. = FALSE)	
+	# number of columns to create for vector (+1 for Total column)
+	total.nodes <- do.call(sum, nodes) + 1
+	# empty vectors with total.nodes columns
+	uniqv <- vector(mode='logical',length=total.nodes)
+	# first element should never be 1
+	cnt <- nodes[[1]]+1
+	uniqv[1:cnt] <- TRUE
+	for (i in 2:length(nodes)) {
+		sindx <- do.call(sum, nodes[1:i-1]) + 1
+		maxj <- length(nodes[[i]])
+		if (mdebug) print(paste("makeuniqv: list element ", i, " start index ", sindx, " length of list element ", maxj))
+		for (j in 1:maxj) {
+			cnt <- nodes[[i]][j]
+			bindx <- sindx + 1
+			lindx <- sindx+cnt
+			if (mdebug) print(paste("makeuniqv: element ", j, " value ", cnt, " bindx ", bindx, " lindx ", lindx))			
+			if (cnt > 1) {
+				uniqv[bindx:lindx] <- TRUE
+			} else {
+				uniqv[bindx] <- FALSE
+			}
+			sindx <- sindx+cnt
+		}
+	}
+	return(uniqv)
+}
+
+makemapv <- function(y,nodes,uniqv) {
+	if (!is.ts(y)) stop("Argument y must be a time-series object.", call. = FALSE)
+	mdebug <- FALSE
+	if (length(nodes[[1]]) == 1 && nodes[[1]][1] == 1) stop("Incorrect nodes list, the first element should not be 1", call. = FALSE)
+	# number of columns to create for vector (+1 for Total column)
+	total.nodes <- do.call(sum, nodes) + 1
+	# empty vectors with total.nodes columns
+	mapv <- vector(mode='integer',length=total.nodes)
+	
+	# first element should never be 1 as hts does not like it
+	mapv[1] <- 0
+	tmpv <- vector()
+	maxi <- length(nodes)
+	for (i in 2:maxi) {
+		ln <- i - 2
+		sindx <- if (i > 2) do.call(sum, nodes[1:ln]) else 1
+		maxj <- length(nodes[[i]])
+		if (mdebug) print(paste("makemapv: list element ", i, " start index ", sindx, " length of list element ", maxj))
+		for (j in 1:maxj) {
+			curr <- sindx + j			
+			cnt <- nodes[[i]][j]
+			if (mdebug) print(paste("makemapv: element ", j, " value ", cnt, " curr index ", curr))			
+			if (cnt == 1) {
+				tmpv <- c(tmpv,curr)
+			} 
+		}
+	}
+	mapv[!uniqv] <- tmpv
+	return(mapv)
+}
